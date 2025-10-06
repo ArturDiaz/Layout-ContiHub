d11373b861d21d2403985fb2250212a280d82e69
ArturDiaz <arturdiazcahuana123@gmail.com>
2025-10-06 16:50:43 -0500

    AD plantilla asincrona

--- Content/js/js-ct.js
+++ Content/js/js-ct.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
     /* cargando paginas */
-    $("#libreria-icons-svg").load("Views/Topic/libreria-icons.html").hide();
+    const basePath = getBasePath();
+    $("#libreria-icons-svg").load(`${basePath}Views/Shared/libreria-icons.html`).hide();
+    $("#headerPageTop").load(`${basePath}Views/Shared/_headerPageTop.html`, function() {
+        $(".AvatarUser").load(`${basePath}Views/Shared/_avatarUser.html`);
+        $(".logo-ct").load(`${basePath}Views/Shared/_logo.html`);
+    });
+    $("#menuSidebarLeft").load(`${basePath}Views/Shared/_menuSidebarLeft.html`, function() {
+        $(".logo-ct").load(`${basePath}Views/Shared/_logo.html`);
+        
+        setupDynamicLinks();
 
-    /* SideBar */
-    $("#expanded-sidebar").on('change', function(){
-        $("body").toggleClass("sidebar-checked", $(this).is(':checked'));
+        /* SideBar */
+        $("#expanded-sidebar").on('change', function(){
+            $("body").toggleClass("sidebar-checked", $(this).is(':checked'));
+        });
+        $(".btn-collapse-js button").on("click", function() {
+            $('#expanded-sidebar').prop('checked', true);
+            $("body").addClass("sidebar-checked");
+        });
     });
-    $(".btn-collapse-js button").on("click", function() {
-        $('#expanded-sidebar').prop('checked', true);
-        $("body").addClass("sidebar-checked");
-    });
+    $("#footer").load(`${basePath}Views/Shared/_footer.html`);
+    
+
 
     /* Nombre de pagina en class */
     var path = window.location.pathname;
@@ -22,3 +35,42 @@ $(document).ready(function(){
     });
 
 })
+
+function getBasePath() {
+    const currentPath = window.location.pathname;
+    if (currentPath === '/' || currentPath === '/index.html') {
+        return '';
+    }
+    
+    // Si estamos en Views/Pages/
+    if (currentPath.includes('/Views/Pages/')) {
+        return '../../';
+    }
+    // Si estamos en Views/
+    if (currentPath.includes('/Views/')) {
+        return '../';
+    }
+    return '';
+}
+
+function setupDynamicLinks() {
+    const basePath = getBasePath();
+    
+    // Recorrer todos los enlaces con clase dynamic-link
+    $('.dynamic-link').each(function() {
+        const originalHref = $(this).attr('href');
+        let newHref = originalHref;
+        
+        // Si es index.html, va a la raíz
+        if (originalHref === 'index.html') {
+            newHref = `${basePath}index.html`;
+        } 
+        // Si es cualquier otra página, va a Views/Pages/
+        else if (originalHref.endsWith('.html')) {
+            newHref = `${basePath}Views/Pages/${originalHref}`;
+        }
+        
+        // Actualizar el href
+        $(this).attr('href', newHref);
+    });
+}
